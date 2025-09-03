a2c5af4a9d82bfa1270160fdc5d2ef7bb289c873
Mashpoe <mashpoesemail@gmail.com>
2025-09-03 05:44:31 -0600

    rename class, update to modern c++, add new features

--- MashPool.h
+++ MashPool.h
@@ -1,6 +1,4 @@
-#ifndef THREAD_POOL_H
-#define THREAD_POOL_H
-
+#pragma once
 #include <vector>
 #include <queue>
 #include <memory>
@@ -9,90 +7,136 @@
 #include <condition_variable>
 #include <future>
 #include <functional>
-#include <stdexcept>
+#include <cassert>
 
-class ThreadPool {
+class MashPool
+{
 public:
-    ThreadPool(size_t);
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::result_of<F(Args...)>::type>;
-    ~ThreadPool();
+
+	MashPool(size_t threadCount = std::thread::hardware_concurrency());
+
+	// joins all worker threads
+	~MashPool();
+
+	void addTask(std::function<void()> task);
+
+	template<class F, class... Args>
+	auto addTaskFuture(F&& f, Args&&... args)
+		-> std::future<std::invoke_result_t<F, Args...>>;
+
+	// waits until all tasks have been completed
+	void wait();
+
 private:
-    // need to keep track of threads so we can join them
-    std::vector< std::thread > workers;
-    // the task queue
-    std::queue< std::function<void()> > tasks;
-    
-    // synchronization
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    bool stop;
+
+	std::vector<std::thread> workers;
+
+	std::queue<std::function<void()>> tasks;
+	std::atomic_uint taskCount;
+
+	// synchronization
+	std::mutex queueMutex;
+	std::condition_variable condition;
+	bool stop;
 };
- 
-// the constructor just launches some amount of workers
-inline ThreadPool::ThreadPool(size_t threads)
-    :   stop(false)
+
+inline MashPool::MashPool(size_t threadCount)
+	: stop{ false }
 {
-    for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
-            [this]
-            {
-                for(;;)
-                {
-                    std::function<void()> task;
+	for (size_t i = 0; i < threadCount; ++i)
+	{
+		workers.emplace_back(
+			[this]
+			{
+				for (;;)
+				{
+					std::function<void()> task;
 
-                    {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty())
-                            return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
-                    }
+					{
+						std::unique_lock<std::mutex> lock{ this->queueMutex };
+						this->condition.wait(lock,
+							[this] {
+								return this->stop || !this->tasks.empty();
+							}
+						);
 
-                    task();
-                }
-            }
-        );
+						if (this->stop && this->tasks.empty())
+						{
+							return;
+						}
+
+						task = std::move(this->tasks.front());
+						this->tasks.pop();
+					}
+
+					task();
+
+					// decrement taskCount only after completing a task
+					--this->taskCount;
+				}
+			}
+		);
+	}
 }
 
-// add new work item to the pool
+inline MashPool::~MashPool()
+{
+	{
+		std::scoped_lock lock{ queueMutex };
+		stop = true;
+	}
+	condition.notify_all();
+
+	for (std::thread& worker : workers)
+	{
+		worker.join();
+	}
+}
+
+inline void MashPool::addTask(std::function<void()> task)
+{
+	// increment "taskCount" before doing anything
+	++taskCount;
+
+	{
+		std::scoped_lock lock{ queueMutex };
+
+		// don't allow enqueueing after stopping the pool
+		assert(!stop);
+
+		tasks.emplace(task);
+	}
+	condition.notify_one();
+}
+
 template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
+auto MashPool::addTaskFuture(F&& f, Args&&... args)
+-> std::future<std::invoke_result_t<F, Args...>>
 {
-    using return_type = typename std::result_of<F(Args...)>::type;
+	// increment "taskCount" before doing anything
+	++taskCount;
 
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        
-    std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
+	using return_type = std::invoke_result_t<F, Args...>;
 
-        // don't allow enqueueing after stopping the pool
-        if(stop)
-            throw std::runtime_error("enqueue on stopped ThreadPool");
+	// this has to be a shared pointer because an std::function cannot be created from a lambda that captures a non copy-constructible type.
+	// the only way to fix this would be to create a custom std::function.
+	auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
 
-        tasks.emplace([task](){ (*task)(); });
-    }
-    condition.notify_one();
-    return res;
+	std::future<return_type> res = task->get_future();
+	{
+		std::scoped_lock lock{ queueMutex };
+
+		// don't allow enqueueing after stopping the pool
+		assert(!stop);
+
+		tasks.emplace([task = std::move(task)]() { (*task)(); });
+	}
+	condition.notify_one();
+
+	return res;
 }
 
-// the destructor joins all threads
-inline ThreadPool::~ThreadPool()
+inline void MashPool::wait()
 {
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        stop = true;
-    }
-    condition.notify_all();
-    for(std::thread &worker: workers)
-        worker.join();
+	while (taskCount > 0);
 }
-
-#endif
