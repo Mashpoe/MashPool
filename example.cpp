183811da9a720c8c7278b903d1f9eef036eb85e7
Mashpoe <mashpoesemail@gmail.com>
2025-09-03 05:57:27 -0600

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,29 +1,89 @@
+#include "MashPool.h"
 #include <iostream>
-#include <vector>
 #include <chrono>
 
-#include "ThreadPool.h"
+uint64_t getTime()
+{
+	namespace sc = std::chrono;
+	return sc::duration_cast<sc::milliseconds>(sc::system_clock::now().time_since_epoch()).count();
+}
 
+// these examples demonstrate two different ways MashPools might be used,
+// while also measuring some of their overhead in a not-so-thorough benchmark.
 int main()
 {
-    
-    ThreadPool pool(4);
-    std::vector< std::future<int> > results;
+	{
+		std::cout << "running MashPool future..." << std::endl;
 
-    for(int i = 0; i < 8; ++i) {
-        results.emplace_back(
-            pool.enqueue([i] {
-                std::cout << "hello " << i << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "world " << i << std::endl;
-                return i*i;
-            })
-        );
-    }
+		uint64_t before = getTime();
 
-    for(auto && result: results)
-        std::cout << result.get() << ' ';
-    std::cout << std::endl;
-    
-    return 0;
+		MashPool pool{ std::thread::hardware_concurrency() };
+		std::vector<std::future<int>> results;
+
+		for (int i = 0; i < 1000000; ++i)
+		{
+			results.emplace_back(
+				pool.addTaskFuture([i]
+					{
+						return i * i;
+					}
+				)
+			);
+		}
+
+		uint64_t sum = 0;
+		for (auto&& result : results)
+		{
+			sum += result.get();
+		}
+
+		uint64_t after = getTime();
+
+		std::cout << "MashPool future time: " << after - before << std::endl;
+		std::cout << "MashPool future sum: " << sum << std::endl;
+	}
+
+	{
+		std::cout << "running MashPool..." << std::endl;
+
+		uint64_t before = getTime();
+
+		MashPool pool{ std::thread::hardware_concurrency() };
+
+		std::atomic_uint64_t sum = 0;
+
+		for (int i = 0; i < 1000000; ++i)
+		{
+			pool.addTask([&sum, i]
+				{
+					sum += i * i;
+				}
+			);
+		}
+
+		uint64_t after = getTime();
+
+		std::cout << "MashPool time: " << after - before << std::endl;
+		std::cout << "MashPool sum: " << sum << std::endl;
+	}
+
+	{
+		std::cout << "running control..." << std::endl;
+
+		uint64_t before = getTime();
+
+		uint64_t sum = 0;
+
+		// this will just get optimized away in most cases,
+		// but it's good for checking the sum.
+		for (int i = 0; i < 1000000; ++i)
+		{
+			sum += i * i;
+		}
+
+		uint64_t after = getTime();
+
+		std::cout << "control time: " << after - before << std::endl;
+		std::cout << "control sum: " << sum << std::endl;
+	}
 }
